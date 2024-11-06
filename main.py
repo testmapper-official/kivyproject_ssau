91e8d1e52ddffd5a3c32b74f17703a587fb7df4f
thanatalice <67954166+testmapper-official@users.noreply.github.com>
2024-11-07 01:43:34 +0400

    Added file explorer and store the data of cards

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import DictProperty
+from kivy.metrics import dp
 
 from kivymd.app import MDApp
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 from widgets import MedicalCard, TitleBar
 import json
+import shutil
 import os
 
 Window.size = (330, 550)
@@ -15,6 +19,11 @@ class MainApp(MDApp):
     data = DictProperty()
 
     def build(self):
+        self.manager_open = False
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_file_manager, select_path=self.create_card
+        )
+
         if os.path.isfile("data/account.json"):
             with open("data/account.json", 'r', encoding='utf-8') as f:
                 self.data = json.load(f)
@@ -22,14 +31,74 @@ class MainApp(MDApp):
             self.data = {
                 "account": {
                     "avatar": "data/лиссс.png"
-                }
+                },
+                "documents": [],
             }
+
         return Builder.load_file("kv_files/mainapp.kv")
 
-    def createCard(self):
+    def on_start(self):
+        for index in range(len(self.data.documents)):
+            self.add_card(index)
+
+    def add_card(self, index):
+        document = self.data["documents"][index]
         self.root.ids.main_scroll.add_widget(
-            MedicalCard()
+            MedicalCard(name=document["title"], date=document["date"], status=document["status"],
+                        image=document["scan"])
         )
+
+    def exit_file_manager(self, *args):
+        self.file_manager.close()
+        self.manager_open = False
+
+    def open_file_manager(self):
+        self.file_manager.show(
+            os.path.expanduser("~")
+        )
+        self.manager_open = True
+
+    def warn(self, text):
+        MDSnackbar(
+            MDSnackbarText(
+                text=text,
+            ),
+            md_bg_color=self.theme_cls.backgroundColor,
+            pos_hint={"center_x": 0.5},
+            y=dp(50),
+            size_hint_x=0.8,
+            size_hint_y=0.05,
+            padding=dp(-5),
+        ).open()
+
+    def create_card(self, path):
+        self.manager_open = False
+        self.file_manager.close()
+
+        if not os.path.isfile(path):
+            self.warn("Невозможно получить изображение")
+            return
+
+        format = path.split('.')[1]
+        filename = path.split("\\")[-1].split("/")[-1]
+
+        if format not in ["png", "jpg", "jpeg"]:
+            self.warn("Невозможно получить изображение")
+            return
+
+        shutil.copy2(path, "data/images/" + filename)
+
+        document = {
+            "title": "Без названия",
+            "date": "20.02.2000",
+            "status": 0,
+            "scan": "data/images/" + filename,
+        }
+
+        self.data.documents.append(document)
+
+
+        self.add_card(-1)
 
     def on_stop(self):
         with open("data/account.json", "w", encoding='utf-8') as f:
--- widgets/medicalcard.py
+++ widgets/medicalcard.py
@@ -1,21 +1,34 @@
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.lang import Builder
 from kivy.animation import Animation
 from kivy.metrics import dp
 
 from kivymd.uix.card import MDCard
 
+
 class MedicalCard(MDCard):
     name = StringProperty()
     date = StringProperty()
-    status = StringProperty()
+    status = NumericProperty()
     image = StringProperty()
     scan = StringProperty()
-    status_variants = {"хорошо": [0.596, 0.835, 0.556, 1],
-                       "срок истёк": [0.87, 0.27, 0.27, 1]}
+    status_variants = {
+        0: {
+            "verdict": "хорошо",
+            "rgba": [0.596, 0.835, 0.556, 1],
+        },
+        1: {
+            "verdict": "срок истёк",
+            "rgba": [0.87, 0.27, 0.27, 1],
+        },
+        2: {
+            "verdict": "срок истекает",
+            "rgba": [1, 0.6, 0, 1],
+        }
+    }
     Builder.load_file("kv_files/medicalcard.kv")
 
-    def __init__(self, name="Справка", date="01.01.1935", status="хорошо", image="data/лиссс.png", **kwargs):
+    def __init__(self, name="Справка", date="01.01.1935", status=0, image="data/лиссс.png", **kwargs):
         super().__init__(kwargs)
         self.name = name
         self.date = date
@@ -26,7 +39,7 @@ class MedicalCard(MDCard):
         self.set_scan()
 
     def on_press(self, *args) -> None:
-         self.narrow() if self.expanded else self.expand()
+        self.narrow() if self.expanded else self.expand()
 
     def expand(self):
         self.expanded = True
@@ -35,19 +48,19 @@ class MedicalCard(MDCard):
 
     def narrow(self):
         self.expanded = False
-        card_animation = Animation(height=dp(85), duration=.35, t="out_expo")
+        card_animation = Animation(height=dp(95), duration=.35, t="out_expo")
         card_animation.start(self)
 
     def set_scan(self):
         pass
         # self.scan = App.get_running_app().data['documents'].title['scan']
 
-    def set_status(self, status: str):
+    def set_status(self, status: int):
         '''
         Color the right line in two colors: green = good and red = bad
         :param status: "good" or "bad"
         :return the message of color changing
         '''
-        self.md_bg_color = self.status_variants[status]
+        self.md_bg_color = self.status_variants[status]["rgba"]
 
         return "The color changed"
