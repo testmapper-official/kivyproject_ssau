1c1817da7d31956379d928817ec1315703cea4e5
Nigota <postaforjob@gmail.com>
2024-11-06 14:43:36 +0400

    Update medicalcard

--- widgets/medicalcard.py
+++ widgets/medicalcard.py
@@ -1,23 +1,28 @@
 from kivy.properties import StringProperty
-from kivymd.uix.card import MDCard
 from kivy.lang import Builder
 from kivy.animation import Animation
-from kivy.app import App
 from kivy.metrics import dp
 
+from kivymd.uix.card import MDCard
 
 class MedicalCard(MDCard):
-    title = StringProperty()
-    desc = StringProperty()
+    name = StringProperty()
+    date = StringProperty()
+    status = StringProperty()
+    image = StringProperty()
     scan = StringProperty()
-    status_variants = {"good": [0.596, 0.835, 0.556, 1],
-                       "bad": [0.87, 0.27, 0.27, 1]}
+    status_variants = {"хорошо": [0.596, 0.835, 0.556, 1],
+                       "срок истёк": [0.87, 0.27, 0.27, 1]}
     Builder.load_file("kv_files/medicalcard.kv")
 
-    def __init__(self, **kwargs):
+    def __init__(self, name="Справка", date="01.01.1935", status="хорошо", image="data/лиссс.png", **kwargs):
         super().__init__(kwargs)
+        self.name = name
+        self.date = date
+        self.status = status
         self.expanded = False
-        self.set_status("bad")
+        self.set_status(status)
+        self.image = image
         self.set_scan()
 
     def on_press(self, *args) -> None:
